edabf7a4d72efae8df56bcc40479116fede735f6
Liang Shennhen <641831191@qq.com>
2022-11-19 18:58:53 +0800

    Day20221119_Python学习记录

---
+++ Day20221119/Day20221119.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    """
+    （函数说明文档，只要打三引号和回车，Pycharm会帮我们快捷填充格式）
+    add函数可以接收两个参数，进行两数相加的功能
+    :param x:待求和数
+    :param y:待求和数
+    :return:两数之和
+    """
+    return x+y
+
+print(add(3, 2))
