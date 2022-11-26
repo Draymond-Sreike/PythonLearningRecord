a0fc76aabcb9211ed94fdd40637ee0ee95c35134
Liang Shennhen <641831191@qq.com>
2022-11-26 20:23:42 +0800

    Day20221126_Python学习日志

---
+++ PythonProject/Day20221126.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+# my_Set = set()
+# print(my_Set)
+# print(type(my_Set))
+
+# my_List = ["我", "是", "南"]
+# print(my_List.pop())
+
+my_Set = {"我", "是", "南"}
+print(my_Set.pop())
