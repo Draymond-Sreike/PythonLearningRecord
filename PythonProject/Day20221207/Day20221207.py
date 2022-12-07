902ae4a6c5860bb953512edbc0907bfeaa540ae1
Liang Shennhen <641831191@qq.com>
2022-12-07 20:19:21 +0800

    Day20221207_Python学习日志

---
+++ PythonProject/Day20221207/Day20221207.py
@@ -0,0 +1,16 @@
+# from ModuleTest import moduleAdd
+#
+# moduleAdd(1, 2)
+#
+# def moduleAdd(a, b):
+#     print("我是当前模块的的函数")
+#
+# moduleAdd(1, 2)
+#
+# from ModuleTest import moduleAdd
+#
+# moduleAdd(2, 4)
+from ModuleTest import *
+print(module_num1)
+print(module_num2)
+moduleAdd(1, 2)
---
+++ PythonProject/Day20221207/ModuleTest.py
@@ -0,0 +1,7 @@
+# __all__ = ['module_num1']
+
+def moduleAdd(a, b):
+    print("我是ModuleTest模块的函数")
+
+module_num1 = 10
+module_num2 =20
